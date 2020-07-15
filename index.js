7710200b664746403e726228cc0b6e5ed99e242b
Anton Shramko <ashramko.exceedteam@gmail.com>
2020-07-15 13:19:00 +0300

    adding todo on click enter

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const bodyParser = require('body-parser');
 const cors = require('cors')
-// const todoRoutes = require('./routes/todos') 
 const todoRoutes = express.Router();
 
 const PORT = 3003
-
 const app = express()
 
 app.use(cors());
@@ -16,21 +14,21 @@ app.use(todoRoutes)
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://anton:anton@cluster0.iruqf.mongodb.net/todos?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-        })
-        
+        await mongoose.connect(
+            'mongodb+srv://anton:anton@cluster0.iruqf.mongodb.net/todos?retryWrites=true&w=majority', {
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true
+            }
+        )
         app.listen(PORT, () => {
             console.log("server has been started on port 3003");   
         })
-        
     } catch (e) {
-        console.log(e);
-        
+        console.log(e);    
     }
 }
+
 start()
 
 todoRoutes.route('/').get(function (req, res) {
@@ -43,24 +41,16 @@ todoRoutes.route('/').get(function (req, res) {
     });
 });
 
-// todoRoutes.route('/').post(function (req, res) {
-//     let todo = new Todo({title: req.body.title});
-//     todo.save().then(todo => res.send(todo))
-// })
-
-
 todoRoutes.route('/').post(function (req, res) {
     console.log(req.body);
     let todo = Todo.create({title: req.body.data.title})
-    
-    // console.log([todo.title, todo.completed, todo.id]);
-        .then((todo) => {
-            console.log(todo);
-            res.status(200).json(todo);
-        })
-        .catch(err => {
-            res.status(400).send('adding new Todo failed');
-        });
+    .then((todo) => {
+        console.log(todo);
+        res.status(200).json(todo);
+    })
+    .catch(err => {
+        res.status(400).send('adding new Todo failed');
+    });
 });
 
 todoRoutes.route('/:id').delete(function(req, res) {
@@ -69,15 +59,10 @@ todoRoutes.route('/:id').delete(function(req, res) {
 })
 
 todoRoutes.route('/:id').patch(function(req, res) {
-    console.log(req.body);
     Todo.findByIdAndUpdate(req.params.id, {...req.body.data})
     .then((todo) => res.send('success'))
     .catch((error) => res.send(error))
+    // Todo.updateMany(req.params.id, {...req.body.data})
+    // .then((todo) => res.send('success'))
+    // .catch((error) => res.send(error))
 })
-
-
-// todoRoutes.route('/:id').patch(function(req, res) {
-//     Todo.findByIdAndUpdate(req.params.id, {title: value} )
-//     .then(() => res.json({title: req.body.title}))
-    
-// })
